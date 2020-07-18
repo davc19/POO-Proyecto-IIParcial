bb399d9fc9509f2f74ad060659bd948253e18f04
davc19 <68044518+davc19@users.noreply.github.com>
2020-07-18 10:28:18 -0600

    Cambio el la Super Clase Persona, se cambio a abstracta y se declararón las propiedades asi como un metodo abstracto

--- src/main/java/helpers/Persona.java
+++ src/main/java/helpers/Persona.java
@@ -8,7 +8,18 @@ package helpers;
  * Milton D. Canales
  */
 
-public class Persona 
+public abstract class Persona 
 {
+    //Propiedades
+    private String Nombre;
+    private String Identidad;
+    private String Direccion;
+    private String Edad;
+    private String Email;
+    private String Sexo;
     
+    
+    
+    //Metodo abstracto
+    public abstract String Categoria();
 }
