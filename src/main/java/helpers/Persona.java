c7f143fa22f345956d78b457cc603d74fcf884a4
davc19 <68044518+davc19@users.noreply.github.com>
2020-07-18 10:40:24 -0600

    Se agrego el constructor y los Setters a la Super Clase

--- src/main/java/helpers/Persona.java
+++ src/main/java/helpers/Persona.java
@@ -14,11 +14,46 @@ public abstract class Persona
     private String Nombre;
     private String Identidad;
     private String Direccion;
-    private String Edad;
+    private int Edad;
     private String Email;
-    private String Sexo;
+    private char Sexo;
     
+    //Constructor
+    public Persona()
+    {
+        //Sin Parametos
+    }
     
+    //Setters
+    public void Nombre(String _nombre)
+    {
+        this.Nombre = _nombre;
+    }
+
+    public void Identidad(String _identidad)
+    {
+        this.Identidad = _identidad;
+    }
+
+    public void Direccion(String _direccion)
+    {
+        this.Direccion = _direccion;
+    }
+
+    public void Edad(int _edad)
+    {
+        this.Edad = _edad;
+    }
+
+    public void Email(String _email)
+    {
+        this.Email = _email;
+    }
+
+    public void Sexo(char _sexo)
+    {
+        this.Sexo = _sexo;
+    }
     
     //Metodo abstracto
     public abstract String Categoria();
